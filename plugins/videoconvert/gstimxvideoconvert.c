EdgeFirstAI/imx-gst1.0-plugin

commit e056e4fe6e73bebd4431149e4869ac9c17800f47

g2d: support any supported input format if the output is NV12 by DPU
Signed-off-by: Elliot Chen <elliot.chen@nxp.com>

plugins/videoconvert/gstimxvideoconvert.c
@@ -436,25 +436,6 @@ static gint get_format_conversion_loss(GstBaseTransform * base,
     return G_MAXINT32;
   }
 
-  /* Only OpenCL on DPU platform can convert NV12_10LE to NV12) */
-  if (HAS_DPU ()) {
-    if (in_name == GST_VIDEO_FORMAT_NV12_10BE_8L128 && out_name == GST_VIDEO_FORMAT_NV12)
-      return 0;
-    /* g2d-dpu can do YUY2 to NV12 convert */
-    if (in_name == GST_VIDEO_FORMAT_YUY2
-        && out_name == GST_VIDEO_FORMAT_NV12)
-      return 0;
-    if (in_name == GST_VIDEO_FORMAT_NV12_10LE40
-        && out_name == GST_VIDEO_FORMAT_NV12)
-      return 0;
-    else if (in_name != GST_VIDEO_FORMAT_NV12_10LE40
-        && out_name == GST_VIDEO_FORMAT_NV12) {
-      if (device->device_type != IMX_2D_DEVICE_OCL) {
-        return G_MAXINT32;
-      }
-    }
-  }
-
   /* accept input format immediately without loss */
   if (in_info == out_info) {
     GST_LOG("same format %s", GST_VIDEO_FORMAT_INFO_NAME(in_info));

libs/device-2d/imx_2d_device_g2d.c
@@ -835,20 +835,6 @@ static gboolean imx_g2d_check_conversion (Imx2DDevice *device, GstCaps *input_ca
     }
   }
 
-  /* Check the specified conversion map */
-  if (out_map->gst_video_format == GST_VIDEO_FORMAT_NV12) {
-    if (in_map->gst_video_format == GST_VIDEO_FORMAT_YUY2
-        || in_map->gst_video_format == GST_VIDEO_FORMAT_NV12_10BE_8L128
-        || in_map->gst_video_format == GST_VIDEO_FORMAT_NV12_10LE40) {
-      return TRUE;
-    } else {
-      GST_INFO ("format (%s) -> format (%s) is not supported.",
-          gst_video_format_to_string(in_map->gst_video_format),
-          gst_video_format_to_string(out_map->gst_video_format));
-      return FALSE;
-    }
-  }
-
   return TRUE;
 }
 

commit 521967ed0b622cf8f6a55351d2659951ba60dfa8

MMFMWK-9533 imxvideoconvert: fix the build warning issue
The build warning issue is caused by
commit e056e4fe6e73bebd4431149e4869ac9c17800f47

Signed-off-by: Elliot Chen <elliot.chen@nxp.com>

plugins/videoconvert/gstimxvideoconvert.c
@@ -415,7 +415,6 @@ static gint get_format_conversion_loss(GstBaseTransform * base,
   const GstVideoFormatInfo *in_info = gst_video_format_get_info(in_name);
   const GstVideoFormatInfo *out_info = gst_video_format_get_info(out_name);
   GstImxVideoConvert *imxvct = (GstImxVideoConvert *)(base);
-  Imx2DDevice *device = imxvct->device;
 
   if (!in_info || !out_info)
     return G_MAXINT32;